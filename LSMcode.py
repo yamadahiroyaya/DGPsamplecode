c4a7801a8563b8bfaf14eec4c95287a2c3161537
yamadahiroyaya <issiki1681227@gmail.com>
2024-04-19 15:49:59 +0900

    L1_estimator

--- LSMcode.py
+++ LSMcode.py
@@ -35,11 +35,37 @@ class pcd_LSM:
         plt.scatter(self._pcd_x,self._pcd_y)
         plt.show()
 
-        #print(np.dot(min_n,np.array[x,y]))
-        #plt.contour(x,y,z,[0])
-        #plt.show()
+    def L1_estimator_IRLS(self):
+        n_0=np.asarray([-1,1])
+        c_0=0
+        x_k=np.asarray([self._pcd_x,self._pcd_y])
+        #print(x_k.shape)
+        e_k=np.asarray([np.dot(n_0,x_k)+c_0])
+        w_e_k=1/abs(e_k)
+        print(w_e_k.shape)
+        s=np.sum(w_e_k)
+        #print(s)
+        
+        weighted_m=(np.sum(w_e_k*x_k,axis=1)/s).T#アダマール積
+     
+        weighted_M=np.zeros((2,2))
+        
+        dev_x=x_k[0]-weighted_m[0]
+        dev_y=x_k[1]-weighted_m[1]
+       
+        for num in range(len(dev_x)):
+            weighted_M+=w_e_k[0][num]*np.array([[dev_x[num],dev_y[num]]]).T@np.array([[dev_x[num],dev_y[num]]])
+            #print(np.array([[dev_x[num],dev_y[num]]]).T@np.array([[dev_x[num],dev_y[num]]]))
+        weighted_M/=s
+        print(weighted_M)
+        #weighted_mu,weighted_n=
+
+
+
+
 
         
 pcd_instance=pcd_LSM(pcd1,pcd_x,pcd_y)
 
-pcd_instance.LSM()
+#pcd_instance.LSM()
+pcd_instance.L1_estimator_IRLS()
