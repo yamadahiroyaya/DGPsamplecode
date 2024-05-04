9f086e5b58bf7139b9352a619c9446ddbdfc4096
yamada <yamada.hiroya.d8@elms.hokudai.ac.jp>
2024-05-04 14:39:00 +0900

    complete

--- LSMcode.py
+++ LSMcode.py
@@ -31,35 +31,49 @@ class pcd_LSM:
 
         z = min_n[0]*x+min_n[1]*y-np.dot(min_n,mean)
         # z = 1 の等高線を描く
-        plt.contour(x, y, z, [0])
+        #plt.contour(x, y, z, [0])
         plt.scatter(self._pcd_x,self._pcd_y)
-        plt.show()
+        #plt.show()
+        n_0=np.array([min_n[0],min_n[1]])
+        c_0=np.array(-np.dot(min_n,mean))
+        return n_0 ,c_0
 
-    def L1_estimator_IRLS(self):
-        n_0=np.asarray([-1,1])
-        c_0=0
+    def L1_estimator_IRLS(self,n_0,c_0):
         x_k=np.asarray([self._pcd_x,self._pcd_y])
-        #print(x_k.shape)
-        e_k=np.asarray([np.dot(n_0,x_k)+c_0])
-        w_e_k=1/abs(e_k)
-        print(w_e_k.shape)
-        s=np.sum(w_e_k)
-        #print(s)
+        for num in range(10):
+            e_k=np.asarray(np.dot(n_0,x_k)+c_0)
+            w_e_k=1/abs(e_k)
+            print(w_e_k.shape)
+            s=np.sum(w_e_k)
+            #print(s)
+            
+            weighted_m=(np.sum(w_e_k*x_k,axis=1)/s).T#アダマール積
         
-        weighted_m=(np.sum(w_e_k*x_k,axis=1)/s).T#アダマール積
-     
-        weighted_M=np.zeros((2,2))
+            weighted_M=np.zeros((2,2))
+            
+            dev_x=x_k[0]-weighted_m[0]
+            dev_y=x_k[1]-weighted_m[1]
         
-        dev_x=x_k[0]-weighted_m[0]
-        dev_y=x_k[1]-weighted_m[1]
-       
-        for num in range(len(dev_x)):
-            weighted_M+=w_e_k[0][num]*np.array([[dev_x[num],dev_y[num]]]).T@np.array([[dev_x[num],dev_y[num]]])
-            #print(np.array([[dev_x[num],dev_y[num]]]).T@np.array([[dev_x[num],dev_y[num]]]))
-        weighted_M/=s
-        print(weighted_M)
-        #weighted_mu,weighted_n=
 
+
+        
+            for num in range(len(dev_x)):
+                weighted_M+=w_e_k[num]*np.array([[dev_x[num],dev_y[num]]]).T@np.array([[dev_x[num],dev_y[num]]])
+            weighted_M/=s#分散共分散行列の計算
+            print(weighted_M)
+            weight_mu,weight_n=LA.eig(weighted_M)
+            weighted_mu=np.min(weight_mu)
+            weighted_n=weight_n[:,np.argmin(weight_mu)]
+            n_0=weighted_n
+            c_0=-np.dot(weighted_n,weighted_m)
+            xrange = np.arange(-2, 10, 1)
+            yrange = np.arange(-2, 10, 1)
+            x, y = np.meshgrid(xrange, yrange)
+            z = n_0[0]*x+n_0[1]*y+c_0
+            # z = 1 の等高線を描く
+            plt.contour(x, y, z, [0])
+            plt.scatter(self._pcd_x,self._pcd_y)
+            plt.show()
 
 
 
@@ -67,5 +81,5 @@ class pcd_LSM:
         
 pcd_instance=pcd_LSM(pcd1,pcd_x,pcd_y)
 
-#pcd_instance.LSM()
-pcd_instance.L1_estimator_IRLS()
+n_0,c_0=pcd_instance.LSM()
+pcd_instance.L1_estimator_IRLS(n_0,c_0)
